404e84dddbdb9e0876f3a431a2e39091197c0c34
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-09-08 10:54:05 +0100

    first commit

---
+++ chrispines.rb
@@ -0,0 +1,94 @@
+# puts 1+2
+# puts ("Hello World")
+#
+# puts 1 + 1
+# puts 2*4
+# puts 10 - 5
+# puts 10/2
+#
+#
+# puts 10 * (2+2)
+#
+# puts 100/ (10*2)
+
+
+=begin
+  this is my multi line comments
+   line 2
+   line 3
+=end
+
+#hours in a year program
+
+# insertYear = 2 #can change
+#
+# hoursInAYear = insertYear * 8760
+# puts hoursInAYear
+#
+#
+# #Minutes in a decade
+#
+# numberOfDecades = 1
+#
+# minsInDecade = numberOfDecades * 5259600
+# puts minsInDecade
+
+
+
+#Chapter 3 - Letters
+
+#Strings -
+
+# puts "hello"
+#
+# puts 'practicing'
+#
+# #String Arithemetic
+#
+# puts 'I like'  + " " +  'apple pie'  #spaces
+#
+# puts 'my name is + name'
+#
+# puts 'my name is'  +  'name'
+#
+# puts 'blink' * 4
+#
+# #Numbers and digits
+#
+# puts 12 + 12  #24
+# puts '12' + '12' #1212
+# puts '12 + 12 ' # 12 + 12
+#
+# puts '2'* 5 #22222
+# puts '2 * 5' #2*5
+#
+# puts 'you\re great'
+#
+# #variables
+#
+# name = 'yasmin'
+#
+# puts = 'hi my name is' + name
+#
+# var = 19
+# var2 = var
+#
+# puts ' '
+#
+# var = 5259
+# puts var + var2
+
+#conversions
+ var3 = 2
+ var4 = '5'
+
+ puts var3.to_s + var4 #converting var3 from a interger to a string
+
+ puts var4.to_i + var4 #converts a string into a interger
+
+ puts '20'.to_float
+ puts '99.999'.to_i #99
+
+puts '5 is my favorite number!'.to_i #5
+puts 'Who asked you about 5 or whatever?'.to_i #0 - string starts with letters and so is ignored
+puts 'You did.'.to_f #0.0
