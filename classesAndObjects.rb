1e7fc5124cd429b92930138d4946992474e41862
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-09-22 09:39:01 +0100

    creating classes and objects

---
+++ classesAndObjects.rb
@@ -0,0 +1,19 @@
+#Classes in ruby
+#Class is a custom data type in ruby
+# data type - by creating various attributes. - pages, chapters
+# class is a blueprint of an object aka reperesnted our book
+# https://www.youtube.com/watch?v=sc5RhTIBf4c 
+
+class Book
+  attr_accessor :title, :author, :pages
+
+end
+
+#Creating our objects - Objects are an instance of class
+
+book1 = Book.new() #Telling ruby i want to store the book datatypes in book1
+book1.title = "Harry Potter"
+book1.author = "JK"
+book.pages = 400
+
+puts book1.title
