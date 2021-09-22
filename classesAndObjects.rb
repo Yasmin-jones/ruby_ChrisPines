44d8e500729b23ab5f238462a87c10d4bf830e0b
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-09-22 10:08:31 +0100

    initialise method

--- classesAndObjects.rb
+++ classesAndObjects.rb
@@ -2,7 +2,7 @@
 #Class is a custom data type in ruby
 # data type - by creating various attributes. - pages, chapters
 # class is a blueprint of an object aka reperesnted our book
-# https://www.youtube.com/watch?v=sc5RhTIBf4c 
+# https://www.youtube.com/watch?v=sc5RhTIBf4c
 
 class Book
   attr_accessor :title, :author, :pages
@@ -17,3 +17,17 @@ book1.author = "JK"
 book.pages = 400
 
 puts book1.title
+
+ #initialise method - https://www.youtube.com/watch?v=Js2eKsgHrFo
+class StudentNames
+  attr_accessor :name, :age
+  def initialise(name, age)
+    @name = name
+    @age = age
+  end
+
+end
+
+student1 = StudentNames.new('sasha', 23)
+
+puts student1.name 
